7d5905976cab56775dff015b34ea70cfde761108
DawidMalarzSzkola <dawidmalarzszkola@dawidmalarz.pl>
2020-05-21 10:35:03 +0200

    Zadanie9 Works

---
+++ Temat8/Zadanie9/Zadanie9.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int DziesietnaNaBinarna(int liczba_dziesietna)
+{
+    if (liczba_dziesietna == 0) {
+        return 0;
+    }
+    else
+    {
+        return (liczba_dziesietna % 2 + 10 * DziesietnaNaBinarna(liczba_dziesietna / 2));
+    }
+        
+}
+
+int main()
+{   
+    int liczba_dziesietna;
+    std::cout << "Wpisz liczbe dziesietna: ";
+    std::cin >> liczba_dziesietna;
+    std::cout << "Liczba binarna: ";
+    std::cout << DziesietnaNaBinarna(liczba_dziesietna);
+    return 0;
+}
