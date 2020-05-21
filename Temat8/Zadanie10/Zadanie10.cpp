235525f40141b580838fae6917b4df86290a0ddd
DawidMalarzSzkola <dawidmalarzszkola@dawidmalarz.pl>
2020-05-21 11:09:34 +0200

    Zadanie10 Works

---
+++ Temat8/Zadanie10/Zadanie10.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int Hex2Dec(std::string hex)
+{
+    int dziesietna = strtol(hex.c_str(), NULL, 16);
+
+    return dziesietna;
+}
+
+std::string Dec2Hex(int dziesietna)
+{
+    char heksadecymalna[255];
+    itoa(dziesietna, heksadecymalna, 16);
+    return (std::string) heksadecymalna;
+}
+
+int main()
+{
+    int wybor;
+    std::string liczba_hex;
+    int liczba_dziesietna;
+    std::cout << "Wybierz opcje zmiany liczby: 1. Hex2Dec\n 2. Dec2Hex";
+    std::cin >> wybor;
+
+    switch (wybor) {
+    case 1:
+
+        std::cout << "Podaj liczbe heksadecymalna:";
+        std::cin >> liczba_hex;
+        std::cout << Hex2Dec(liczba_hex);
+        break;
+    case 2:
+        std::cout << "Podaj liczbe dziesietna:";
+        std::cin >> liczba_dziesietna;
+        std::cout << Dec2Hex(liczba_dziesietna);
+        break;
+    }
+    return 0;
+}
